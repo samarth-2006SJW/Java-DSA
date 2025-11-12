04110b447b8055b50644f8f9bf407cae1972dc3f
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-12 14:01:00 +0530

    if-else

---
+++ lec_01/topics.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class topics
+{
+    public static void main(String[] args)
+    {
+        int a,b,sum;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        a=sc.nextInt();
+        System.out.println("Enter second number");
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("Adiition="+sum);
+        sc.close();
+    }
+};
+
