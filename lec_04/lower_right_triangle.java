124965fb0739daba27e8513ad02d8c3228528bc0
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-18 13:30:11 +0530

    Patterns

---
+++ lec_04/lower_right_triangle.java
@@ -0,0 +1,20 @@
+package lec_04;
+import java.util.*;
+public class lower_right_triangle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows:");
+        int n=sc.nextInt();
+        sc.close();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+    
+}
