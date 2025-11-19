8320aae534a77c3e1cce285e00293c087527e1dc
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-19 13:20:31 +0530

    Patterns

---
+++ lec_04/pyramid_point_right.java
@@ -0,0 +1,32 @@
+package lec_04;
+import java.util.*;
+public class pyramid_point_right {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows");
+        int n=sc.nextInt();
+        sc.close();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+        for(int i=0;i<=n;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        for(int i=n;i>0;i--)
+        {
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
