1a1ad35b8d79c44afa95cc583f8c57064d4ccfda
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-26 22:33:01 +0530

    Strings

---
+++ lec_07/string_implementation.java
@@ -0,0 +1,15 @@
+package lec_07;
+import java.util.*;
+public class string_implementation {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=new String();
+        String a=new String();
+        s=sc.next();
+        System.out.print("String ="+s+"\n");
+        a=sc.nextLine();
+        System.out.print("String="+a);
+        sc.close();
+    }
+}
