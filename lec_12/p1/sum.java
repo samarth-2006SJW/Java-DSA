7ee9027361e96a9370d429e35b741d249dda0b98
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-30 12:00:00 +0530

    Concept of packages

---
+++ lec_12/p1/sum.java
@@ -0,0 +1,13 @@
+package lec_12.p1;
+
+public class sum {
+    int a,b;
+    public sum(int a,int b){
+        this.a=a;this.b=b;
+    }
+    public int sumo(){
+        int c=a+b;
+        return c;
+    }
+    
+}
