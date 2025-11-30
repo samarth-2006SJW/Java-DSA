aa8c35a1152a533db3527b44934b02923ddca812
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-30 12:00:00 +0530

    Concept of packages

---
+++ lec_12/p2/division.java
@@ -0,0 +1,12 @@
+package lec_12.p2;
+
+public class division {
+    float a,b;
+    public division(float a,float b){ //if protected to sum.import java main nhi dikhegi
+        this.a=a;this.b=b;
+    }
+    public float div(){
+        float c=a/b;
+        return c;
+    }
+}
