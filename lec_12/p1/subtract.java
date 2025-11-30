eca30efcbaf06aa99b65b81d6a009cb0874504e3
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-11-30 12:00:00 +0530

    Concept of packages

---
+++ lec_12/p1/subtract.java
@@ -0,0 +1,13 @@
+package lec_12.p1;
+
+public class subtract {
+    int a,b;
+    public subtract(int a,int b){
+        this.a=a;
+        this.b=b;
+    }
+    public int sub(){
+        int c=a-b;
+        return c;
+    }
+}
