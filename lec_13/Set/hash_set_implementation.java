f5c4b80d3f12aae0850555b29017a1a7fc238883
samarth-2006SJW <samarthpathrotkar.2006@gmail.com>
2025-12-11 11:08:19 +0530

    Collections Framework

---
+++ lec_13/Set/hash_set_implementation.java
@@ -0,0 +1,12 @@
+package lec_13.Set;
+import java.util.*;
+public class hash_set_implementation {
+    public static void main(String[] args){
+        HashSet<Integer> list=new HashSet<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.print(list);
+    }
+}
